d03adfa7a2d6e8171bd11ca9e8865fd846572dd8
lee <derrosilee@gmail.com>
2022-12-30 05:52:15 +0300

    Added menu view function

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+# To display page with the menu's
+def menu(request):
+    return render(request, 'menu.html')
