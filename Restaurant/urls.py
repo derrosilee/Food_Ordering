f6002fc8bdbd0efcbdcc6417beb0b79b6de9da44
lee <derrosilee@gmail.com>
2022-12-30 07:05:08 +0300

    addd Login url

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="views"),
     path('menu/', views.menu, name="Menu"),
-    path('register/', views.register, name="register")
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login")
 ]
