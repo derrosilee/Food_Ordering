0d123213803a9f1e8ae64fc2d2a3442b27ad2316
lee <derrosilee@gmail.com>
2022-12-30 06:36:04 +0300

    Added Register url

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="views"),
-    path('menu/', views.menu, name="Menu")
+    path('menu/', views.menu, name="Menu"),
+    path('register/', views.register, name="register")
 ]
