73996a339054c1d7348a1d03add3224704a8dc0d
lee <derrosilee@gmail.com>
2022-12-30 07:54:44 +0300

    Changed login bg img

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="views"),
+    path('home/', views.home, name="home"),
     path('menu/', views.menu, name="Menu"),
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login")
