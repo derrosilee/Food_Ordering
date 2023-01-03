c6133862ea77204af1f2cf4d514b84886859b9bc
lee <derrosilee@gmail.com>
2023-01-03 05:44:27 +0300

    added product view

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('menu/', views.menu, name="Menu"),
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login")
+    path('login/', views.login, name="login"),
+    path('product/', views.product, name="product")  # Replace with I'd View
 ]
