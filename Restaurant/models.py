c8f7b4a8824ebcc801fd3ddb2316b9daaa5d9727
lee <derrosilee@gmail.com>
2023-01-04 06:54:47 +0300

    added Menu models fields

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+# Models for the Menu
+class Menu(models.Model):
+    id = models.UUIDField(primary_key=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField()
+    SAUCES_AVAILABLE_CHOICES = [
+        ('Tomato Ketchup', 'Tomato Ketchup'),
+        ('Peri Peri Mayo', 'Peri Peri Mayo'),
+        ('Shito', 'Shito'),
+        ('Honey Mustard Mayo', 'Honey Mustard Mayo'),
+        ('Mango Mayo', 'Mango Mayo'),
+        ('Only The Brave Hot Sauce', 'Only The Brave Hot Sauce'),
+        ('Shata Mayonnaisse', 'Shata Mayonnaisse')
+    ]
+    sauces_available = models.CharField(max_length=40, choices=SAUCES_AVAILABLE_CHOICES, default='Tomato Ketchup')
+
+    def __str__(self):
+        return self.name
