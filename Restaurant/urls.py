74c84811046b4c07cffd52dc1af5d86bdc3c4b45
lee <derrosilee@gmail.com>
2023-01-04 06:27:51 +0300

    added navbar links

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name="home"),
-    path('menu/', views.menu, name="Menu"),
+    path('menu/', views.menu, name="menu"),
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('product/', views.product, name="product")  # Replace with I'd View
