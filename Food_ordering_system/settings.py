09cb9a3306a1e1d85e5b0ba0a7c4f1994e525f16
lee <derrosilee@gmail.com>
2023-01-04 07:12:02 +0300

    added media routes

--- Food_ordering_system/settings.py
+++ Food_ordering_system/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# MEDIA_ROOT is the absolute filesystem path to the directory for user-uploaded files
+# MEDIA_URL is the URL we can use in our templates for the files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
