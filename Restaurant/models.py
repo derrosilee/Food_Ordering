1be81232046c28fba84618b3519a118767957317
lee <derrosilee@gmail.com>
2023-02-13 05:42:16 +0300

    added migrations

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -8,20 +8,21 @@ class Menu(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     description = models.TextField()
+    sauces = models.CharField(max_length=100)
     image_1 = models.ImageField(upload_to='images/food/')
     image_2 = models.ImageField(upload_to='images/food/')
     image_3 = models.ImageField(upload_to='images/food/')
     image_4 = models.ImageField(upload_to='images/food/')
-    SAUCES_AVAILABLE_CHOICES = [
-        ('Tomato Ketchup', 'Tomato Ketchup'),
-        ('Peri Peri Mayo', 'Peri Peri Mayo'),
-        ('Shito', 'Shito'),
-        ('Honey Mustard Mayo', 'Honey Mustard Mayo'),
-        ('Mango Mayo', 'Mango Mayo'),
-        ('Only The Brave Hot Sauce', 'Only The Brave Hot Sauce'),
-        ('Shata Mayonnaisse', 'Shata Mayonnaisse')
-    ]
-    sauces_available = models.CharField(max_length=40, choices=SAUCES_AVAILABLE_CHOICES, default='Tomato Ketchup')
+    # SAUCES_AVAILABLE_CHOICES = [
+    #     ('Tomato Ketchup', 'Tomato Ketchup'),
+    #     ('Peri Peri Mayo', 'Peri Peri Mayo'),
+    #     ('Shito', 'Shito'),
+    #     ('Honey Mustard Mayo', 'Honey Mustard Mayo'),
+    #     ('Mango Mayo', 'Mango Mayo'),
+    #     ('Only The Brave Hot Sauce', 'Only The Brave Hot Sauce'),
+    #     ('Shata Mayonnaisse', 'Shata Mayonnaisse')
+    # ]
+    # sauces_available = models.CharField(max_length=40, choices=SAUCES_AVAILABLE_CHOICES, default='Tomato Ketchup')
 
     def __str__(self):
         return self.name
